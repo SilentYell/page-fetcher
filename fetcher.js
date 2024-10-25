05bc8ca3d6668bd2ffac178602eecbf401d9cae6
Nik Hixson <nikilobe@gmail.com>
2024-10-25 10:31:41 -0700

    creating the downloadFile function to handle downloading the file. added comments throughout. tested

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,5 @@
+/* eslint-disable func-style */
+
 //importing needle, fs, and readline
 const needle = require('needle');
 const fs = require('fs');
@@ -14,7 +16,7 @@ const filePath = process.argv[3];
 //choosing if file exists whether to overwrite or not using rl and then close after response.
 if (fs.existsSync(filePath)) {
   rl.question('File already exists. Overwrite? (Y/N): ', (answer) => {
-    //if answer is not Y then send cancel message
+    //if answer is not Y then send cancel message and close
     if (answer.toUpperCase() !== 'Y') {
       console.log('Download canceled');
       rl.close();
@@ -24,6 +26,36 @@ if (fs.existsSync(filePath)) {
     downloadFile();
   });
 } else {
-  //if file does not exist then download and create file in location
+  //if file does not exist then call downloadFile function
   downloadFile();
+}
+//this function handles the downloading of the file
+function downloadFile() {
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      rl.close();
+      return;
+    }
+
+    // Check for a successful status code
+    if (response.statusCode !== 200) {
+      console.error(`Failed to fetch the URL. Status code: ${response.statusCode}`);
+      rl.close();
+      return;
+    }
+    
+    //write to the filePath. if error occurs write message to inform
+    fs.writeFile(filePath, body, (err) => {
+      if (err) {
+        console.log('An error occurred:', err);
+        rl.close();
+        return;
+      }
+
+      //if file writes successfully then log success message
+      console.log(`Successfully downloaded ${body.length} bytes to ${filePath}`);
+      rl.close();
+    });
+  });
 }
