4f1e5db80e17059a0a9020e83600b18b2effe5a4
Nik Hixson <nikilobe@gmail.com>
2024-10-25 10:11:54 -0700

    Importing needle, fs, and readline

---
+++ fetcher.js
@@ -0,0 +1,9 @@
+//importing needle, fs, and readline
+const needle = require('needle');
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
