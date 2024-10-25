fcddbacb979534ed2b70cf1c6690023b114b5464
Nik Hixson <nikilobe@gmail.com>
2024-10-25 10:19:30 -0700

    set up if statements to handle whether the file exists already and to ask user if they want the file overridden if it exists. if not then create file at location

--- fetcher.js
+++ fetcher.js
@@ -10,3 +10,20 @@ const rl = readline.createInterface({
 //variables created for pulling url and then filePath next
 const url = process.argv[2];
 const filePath = process.argv[3];
+
+//choosing if file exists whether to overwrite or not using rl and then close after response.
+if (fs.existsSync(filePath)) {
+  rl.question('File already exists. Overwrite? (Y/N): ', (answer) => {
+    //if answer is not Y then send cancel message
+    if (answer.toUpperCase() !== 'Y') {
+      console.log('Download canceled');
+      rl.close();
+      return;
+    }
+    //if response is Y then overwrite file
+    downloadFile();
+  });
+} else {
+  //if file does not exist then download and create file in location
+  downloadFile();
+}
